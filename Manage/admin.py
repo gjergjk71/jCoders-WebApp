0d2c90cdc4334dacf643b2f2de1a962e656133dc
gjergjk71 <gjergjk71@gmail.com>
2018-06-06 19:40:10 +0200

    Added Payment to admin.site.register

--- Manage/admin.py
+++ Manage/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Module,Training,Group,Student,Event,Trainer
+from .models import Module,Training,Group,Student,Event,Trainer,Payment
 # Register your models here.
 
 admin.site.register(Module)
@@ -8,3 +8,4 @@ admin.site.register(Group)
 admin.site.register(Student)
 admin.site.register(Trainer)
 admin.site.register(Event)
+admin.site.register(Payment)
