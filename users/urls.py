32862091b4047f9ee28df1f765bb1b2ee721c658
gjergjk71 <gjergjk71@gmail.com>
2018-06-12 14:17:02 +0200

    Created a path for views.change_password

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path("logout/", auth_views.logout,{'next_page': '/users/login'},name="logout"),
 	path("account/", views.account,name="account"),
 	path("account/edit/<id>", views.editAccount,name="editAccount"),
+	path("account/change_password", views.change_password,name="change_password"),
 	path("", views.index, name="index"),
 	path("payments/", views.showPayments, name="payments"),
 	path("attendances/", views.showAttendances, name="attendances"),
