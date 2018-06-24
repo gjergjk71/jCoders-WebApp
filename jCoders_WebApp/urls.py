1fa9d174f534abd5e420ea91af7a1ce164042ccb
gjergjk71 <gjergjk71@gmail.com>
2018-06-24 21:13:14 +0200

    Mapped users.urls to "/"

--- jCoders_WebApp/urls.py
+++ jCoders_WebApp/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("users.urls"))
+    path('', include("users.urls")),
 ]
